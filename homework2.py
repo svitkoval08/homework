335d10467deb49a3dfcaaa23992b6fa566e494b4
Svitlana Koval <123551045+svitkoval08@users.noreply.github.com>
2023-02-07 18:49:38 +0200

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+property_transfer_xml = """
+<con:name>AuthTocken</con:name><con:sourceType>Response</con:sourceType><con:sourceStep>login</con:sourceStep><con:sourcePath>declare namespace ns1='urn:Magento';
+//ns1:loginResponse/loginReturn[1]</con:sourcePath><con:targetType>Request</con:targetType><con:targetStep>multiCall</con:targetStep><con:targetPath>declare namespace urn='urn:Magento';
+//urn:multiCall/sessionId['?']</con:targetPath>
+"""
+result=property_transfer_xml.split('con:targetType')
+print('Результат:', result)
