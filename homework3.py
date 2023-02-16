6e9aa1f72cbc3e5001beff508a6cd67791cd47ba
Svitlana Koval <123551045+svitkoval08@users.noreply.github.com>
2023-02-16 09:46:29 +0200

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,11 @@
+import random
+print("---Кафе “У Монті”---")
+dishes = input("Введіть список страв, що треба подати до вечері(розділені комами): ")
+dishes = dishes.split(',')
+total_price = 0
+for dish in dishes:
+    price = random.randint(1, 5000)
+    print("{:<15}{:>5} грн".format(dish, price))
+    total_price += price
+print("Всього {:>5} грн".format(total_price))
+
